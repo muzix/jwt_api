32de32420e7f90e0b8a93e15113aaa3841779924
sonpt1986 <68746293+sonpt1986@users.noreply.github.com>
2020-08-04 23:24:22 +0700

    update kill all background ffmpeg

--- deploy/core.sh
+++ deploy/core.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+cleanup() {
+        local pids=$(jobs -pr)
+        echo $pids >> /root/debugxx.log
+        [ -n "$pids" ] && kill -9 $pids
+}
+
+trap cleanup EXIT HUP INT QUIT PIPE TERM
+
 /usr/bin/ffmpeg -threads 3 -i rtmp://localhost/live/$1 -c:v libx264 -preset:v ultrafast -vf addroi=iw/4:ih/4:iw/2:ih/2:-1/10 -force_key_frames "expr:gte(t,n_forced*0.5)" -s 1280x720 -b:v 2000K -c:a copy -f flv rtmp://localhost/hls/$1_hi -c:v libx264 -preset:v ultrafast -vf addroi=iw/4:ih/4:iw/2:ih/2:-1/10 -force_key_frames "expr:gte(t,n_forced*0.5)" -s 854x480 -b:v 700K -c:a copy -f flv rtmp://localhost/hls/$1_low 1>>/root/run.log 2>>/root/error.log &
 echo $2 > /root/debug.log
 arr=(${2//[ ]/ })
@@ -20,5 +30,3 @@ else
   /usr/bin/ffmpeg -threads 3 -i rtmp://localhost/live/$1 -c:v copy -c:a copy -f flv "rtmps://live-api-s.facebook.com:443/rtmp/${arr[2]}" >> /root/debug.log &
 fi
 wait
-pkill -P $$
-echo "All done" >> /root/debug.log
